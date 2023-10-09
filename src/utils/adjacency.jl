4d59f4522a0da15796867df28056d78c227e969d
Erick <eral.th07@gmail.com>
2023-10-09 17:00:05 +0300

    consistency in adjacency

--- src/utils/adjacency.jl
+++ src/utils/adjacency.jl
@@ -1,28 +1,29 @@
 """
-    adjacency(x::CartesianGrid; order = 1, cyclic = false)
+    adjacency(d, circular = false)
 
-Return the adjacency `n×n` matrix (A) of the CartesianGrid `x` for a specified `order`.
+Return a sparse `n×n` adjacency matrix A for a domain `d`, where `n` represents the
+domain's size.
 
-`n` is the number of elements of the `x`. The value of A[i,j] is `true` if the element `i`
-and `j` are neighbors of the specified `order`, and A[i, j] is `false` otherwise. Use the
-argument `cyclic = true` to assume neighbors defined by embedding the CartesianGrid in a
-torus.
+The value of A[i,j] is `true` if the elements `i` and `j` are first-order neighbors;
+otherwise, it is false. Supported domain types include `CartesianGrid`, `GeometrySet`, or
+`SimpleGraph`, with an option for CartesianGrid to treat the grid as circular (`circular =
+true`), allowing neighbors to wrap around the grid's edges like a toroidal topology.
 """
-function adjacency(g::CartesianGrid{Dim}; order::Int = 1, cyclic::Bool = false) where {Dim}
+function adjacency(d::CartesianGrid{Dim}; circular::Bool = false) where {Dim}
     if Dim == 1
-        adjacency1(g; order = order, cyclic = cyclic)
+        adjacency1(d; order = 1, circular = circular)
     elseif Dim == 2
-        adjacency2(g; order = order, cyclic = cyclic)
+        adjacency2(d; order = 1, circular = circular)
     else
         throw(ErrorException("adjacency is not implemented for Dim > 2"))
     end
 end
 
-function adjacency1(g::CartesianGrid{1}; order::Int = 1, cyclic::Bool = false)
-    n = nelements(g)
+function adjacency1(d::CartesianGrid{1}; order::Int = 1, circular::Bool = false)
+    n = nelements(d)
 
     # neighbors to the right (→)
-    if cyclic
+    if circular
         Ir = 1:n
         Jr = mod1.(Ir .+ order, n)
     else
@@ -35,16 +36,16 @@ function adjacency1(g::CartesianGrid{1}; order::Int = 1, cyclic::Bool = false)
     A .| A'
 end
 
-function adjacency2(g::CartesianGrid{2}; order::Int = 1, cyclic::Bool = false)
-    n1, n2 = size(g)
-    n = nelements(g)
+function adjacency2(d::CartesianGrid{2}; order::Int = 1, circular::Bool = false)
+    n1, n2 = size(d)
+    n = nelements(d)
 
     # function to convert i,j cel to k-index (CartesianGrid)
     function ij_to_k(i, j, n1, n2)
         (j - 1) * n1 + i
     end
 
-    if cyclic
+    if circular
         # neighbors to the right (→)
         Ir = [ij_to_k(i, j, n1, n2) for i = 1:n1 for j = 1:n2]
         Jr = [ij_to_k(mod1(i + order, n1), j, n1, n2) for i = 1:n1 for j = 1:n2]
@@ -66,21 +67,24 @@ function adjacency2(g::CartesianGrid{2}; order::Int = 1, cyclic::Bool = false)
 end
 
 function adjacency(d::GeometrySet)
+    # initialize
     n = length(d)
     rows = Int64[]
     cols = Int64[]
 
+    # neighbors
     for i = 1:n
         for j = (i+1):n
-            if hasintersect(d[i], d[j])
+            if intersects(d[i], d[j])
                 push!(rows, i)
                 push!(cols, j)
             end
         end
     end
 
+    # sparse adjacency matrix
     A = sparse(rows, cols, true, n, n)
-    A + A'
+    A .| A'
 end
 
-adjacency(g::SimpleGraph) = adjacency_matrix(g)
+adjacency(d::SimpleGraph) = adjacency_matrix(d, Bool)
